b28b048cf14ffd920158a5f0a44f826847a59257
NathanDRibeiro <nathandiasribeiro2@gmail.com>
2025-06-25 11:02:22 -0300

    Create 1014-Consumo.c

---
+++ 1014-Consumo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int X;
+    double Y;
+    double consumoMedio;
+
+    scanf("%d", &X);
+    scanf("%lf", &Y);
+
+    consumoMedio = X / Y;
+
+    printf("%.3lf km/l\n", consumoMedio);
+
+    return 0;
+}
